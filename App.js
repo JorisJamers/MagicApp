b895b720be8539c76272a67028a072d4155413e8
Joris Jamers <jorisjamers@joriss-mbp.home>
2018-08-16 18:49:11 +0200

    Centered counters

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { StyleSheet, Text, Button, Alert, View } from 'react-native';
 
@@ -6,6 +7,9 @@ export default class App extends React.Component {
     return (
       <View style={styles.background}>
         <View style={{flex: 1, backgroundColor: 'skyblue'}} >
+          <Text style={styles.loyaltyCounterTop}>
+            0
+          </Text>
           <Text style={styles.mainCounterTop}>
             20
           </Text>
@@ -13,6 +17,9 @@ export default class App extends React.Component {
         <View style={{flex: 1, backgroundColor: 'darkgrey'}} >
           <Text style={styles.mainCounterBottom}>
             20
+          </Text>
+          <Text style={styles.loyaltyCounterBottom}>
+            0
           </Text>
         </View>
       </View>
@@ -25,16 +32,32 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  mainCounterBottom: {
-    color: 'white',
-    fontSize: 30,
-    alignItems: 'center',
-  },
   mainCounterTop: {
     color: 'white',
-    fontSize: 30,
+    fontSize: 100,
     alignItems: 'center',
-    transform: [{ rotate: '180deg'}]
+    transform: [{ rotate: '180deg'}],
+    paddingLeft: 130,
+    paddingTop: 120,
   },
-
+  loyaltyCounterTop: {
+    color: 'white',
+    fontSize: 100,
+    alignItems: 'center',
+    transform: [{ rotate: '180deg'}],
+    paddingLeft: 160,
+    paddingBottom: 50,
+  },
+  mainCounterBottom: {
+    color: 'white',
+    fontSize: 100,
+    alignItems: 'center',
+    paddingLeft: 130,
+  },
+  loyaltyCounterBottom: {
+    color: 'white',
+    fontSize: 100,
+    alignItems: 'center',
+    paddingLeft: 160,
+  }
 });
