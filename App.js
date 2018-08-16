07666a01fa6fcb85dc5d0eb6b70675b5534acd12
Joris Jamers <jorisjamers@joriss-mbp.home>
2018-08-16 19:20:47 +0200

    Buttons added + and -

--- App.js
+++ App.js
@@ -3,24 +3,77 @@ import React from 'react';
 import { StyleSheet, Text, Button, Alert, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  _onPressButton() {
+  Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
       <View style={styles.background}>
         <View style={{flex: 1, backgroundColor: 'skyblue'}} >
-          <Text style={styles.loyaltyCounterTop}>
-            0
-          </Text>
-          <Text style={styles.mainCounterTop}>
-            20
-          </Text>
+          <View style={styles.topLoyaltyView}>
+            <Button
+              onPress={this._onPressButton}
+              title="-"
+              color="#841584"
+            />
+            <Text style={styles.loyaltyCounterTop}>
+              0
+            </Text>
+            <Button
+              onPress={this._onPressButton}
+              title="+"
+              color="#841584"
+            />
+          </View>
+          <View style={styles.topCounterView}>
+            <Button
+              onPress={this._onPressButton}
+              title="-"
+              color="#841584"
+            />
+            <Text style={styles.mainCounterTop}>
+              20
+            </Text>
+            <Button
+              onPress={this._onPressButton}
+              title="+"
+              color="#841584"
+            />
+          </View>
         </View>
         <View style={{flex: 1, backgroundColor: 'darkgrey'}} >
-          <Text style={styles.mainCounterBottom}>
-            20
-          </Text>
-          <Text style={styles.loyaltyCounterBottom}>
-            0
-          </Text>
+          <View style={styles.bottomCounterView}>
+            <Button
+              onPress={this._onPressButton}
+              title="+"
+              color="#841584"
+            />
+            <Text style={styles.mainCounterBottom}>
+              20
+            </Text>
+            <Button
+              onPress={this._onPressButton}
+              title="-"
+              color="#841584"
+            />
+          </View>
+          <View style={styles.bottomLoyaltyView}>
+            <Button
+              onPress={this._onPressButton}
+              title="+"
+              color="#841584"
+            />
+            <Text style={styles.loyaltyCounterTop}>
+              0
+            </Text>
+            <Button
+              onPress={this._onPressButton}
+              title="-"
+              color="#841584"
+            />
+          </View>
         </View>
       </View>
     );
@@ -32,32 +85,47 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
+  topLoyaltyView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  topCounterView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomCounterView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  bottomLoyaltyView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   mainCounterTop: {
     color: 'white',
     fontSize: 100,
     alignItems: 'center',
     transform: [{ rotate: '180deg'}],
-    paddingLeft: 130,
-    paddingTop: 120,
   },
   loyaltyCounterTop: {
     color: 'white',
     fontSize: 100,
     alignItems: 'center',
     transform: [{ rotate: '180deg'}],
-    paddingLeft: 160,
-    paddingBottom: 50,
   },
   mainCounterBottom: {
     color: 'white',
     fontSize: 100,
     alignItems: 'center',
-    paddingLeft: 130,
   },
-  loyaltyCounterBottom: {
-    color: 'white',
-    fontSize: 100,
-    alignItems: 'center',
-    paddingLeft: 160,
+  buttonContainer: {
+    flexDirection: 'row',
+    paddingBottom: 0,
   }
 });
